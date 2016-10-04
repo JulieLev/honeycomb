14406e6fcfa8e27f72e035418028b6873b033bca
JulieLev <jkgee1@yahoo.co.uk>
2016-10-04 23:33:28 +0100

    order has full cost

--- lib/order.rb
+++ lib/order.rb
@@ -1,10 +1,16 @@
 class Order
 
+  attr_reader :deliveries
+
   def initialize(deliveries = [])
     @deliveries = deliveries
   end
 
   def full_cost
     @deliveries.inject(0) { |sum, delivery| sum + delivery.cost }
+  end
+
+  def add_delivery(delivery)
+    @deliveries << delivery
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,9 +3,33 @@ describe 'Order' do
 
   subject(:order) { Order.new }
 
-  context '#cost' do
+  context 'when initialized' do
+    it 'has no deliveries' do
+      expect(order.deliveries.count).to eq 0
+    end
+
     it 'has a default full cost of zero' do
       expect(order.full_cost).to eq 0
+    end
+  end
+
+  context 'when deliveries are added' do
+    let(:standard_delivery) { double :delivery, cost: 10 }
+
+    before do
+      2.times { order.add_delivery(standard_delivery) }
+    end
+
+    describe '#add_delivery' do
+      it 'adds each delivery to the order' do
+        expect(order.deliveries.count).to eq 2
+      end
+    end
+
+    describe '#cost' do
+      it 'has a full cost from the sum of the delivery costs' do
+        expect(order.full_cost).to eq 20
+      end
     end
   end
 end

2a923898b8e767e2588c93906549ab6c66c02086
JulieLev <jkgee1@yahoo.co.uk>
2016-10-04 23:55:09 +0100

    order knows number of delivery types included

--- lib/order.rb
+++ lib/order.rb
@@ -13,4 +13,12 @@ class Order
   def add_delivery(delivery)
     @deliveries << delivery
   end
+
+  def standard_delivery_count
+    @deliveries.count { |delivery| delivery.type == 'Standard' }
+  end
+
+  def express_delivery_count
+    @deliveries.count { |delivery| delivery.type == 'Express' }
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,6 +2,8 @@ require 'order'
 describe 'Order' do
 
   subject(:order) { Order.new }
+  let(:standard_delivery) { double :delivery, cost: 10, type: 'Standard' }
+  let(:express_delivery) { double :delivery, cost: 20, type: 'Express' }
 
   context 'when initialized' do
     it 'has no deliveries' do
@@ -13,9 +15,7 @@ describe 'Order' do
     end
   end
 
-  context 'when deliveries are added' do
-    let(:standard_delivery) { double :delivery, cost: 10 }
-
+  context 'when two standard deliveries are added' do
     before do
       2.times { order.add_delivery(standard_delivery) }
     end
@@ -26,10 +26,28 @@ describe 'Order' do
       end
     end
 
-    describe '#cost' do
+    describe '#full_cost' do
       it 'has a full cost from the sum of the delivery costs' do
         expect(order.full_cost).to eq 20
       end
     end
+
+    describe '#standard_delivery_count' do
+      it 'knows how many standard deliveries are in the order' do
+        expect(order.standard_delivery_count).to eq 2
+      end
+    end
+
+    describe '#express_delivery_count' do
+      it 'knows how many express deliveries are in the order' do
+        expect(order.express_delivery_count).to eq 0
+      end
+    end
   end
+
+  # context 'when order warrants discounts' do
+  #   before do
+  #     2.times { order.add_delivery(express_delivery) }
+  #   end
+  # end
 end
